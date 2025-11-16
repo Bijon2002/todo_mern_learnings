6eeb7945e34ba23d2505a5682d0dc4b687346c53
bijon2002 <bijosilin02@gmail.com>
2025-11-17 01:25:28 +0530

    update method

--- server.js
+++ server.js
@@ -1,35 +1,116 @@
 //express
 
 const express = require('express');
+const mongoose = require ('mongoose');
 
-//create an instance of express
+//create an instance of expres
 const app = express();
 
 
 app.use (express.json());
 
     //sample in memory storage for todo items
-    let todos =[];
+  //  let todos =[];
 
     
-
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/mernaapp')
+.then(()=>
+{
+    console.log('connected to mongodb');
+})
+.catch((err)=>
+{
+    console.error('failed to connect to mongodb', err);
+});
 //create a new todo item
 
-app.post('/todos' ,(req,res)=>
+//create scehema
+const todoSchema = new mongoose.Schema({
+    title:{type:String, required:true},
+    description: String
+});
+
+//create model
+const Todomodel = mongoose.model('Todo', todoSchema);
+
+
+app.post('/todos' ,async (req,res)=>
 {
-    const {title,description}=req.body;
-    const newTodo = {
-        id:todos.length+1,
-        title,
-        description
-    };
+   const {title,description}=req.body;
+  //  const newTodo = {
+  //      id:todos.length+1,
+   //     title,
+  //      description
+  //  };
 
-todos.push(newTodo);
-console.log(todos);
+//todos.push(newTodo);
+//console.log(todos);
+try
+{
+   const newTodo =  new Todomodel({title,description});
 
-res.status(201).json(newTodo);
+   await newTodo.save();
+   res.status(201).json(newTodo);
+}
+catch(err)
+{
+  console.log('error creating todo', err);
+    res.status(500).json({message:err.message});
+}
+
+
+
 
     })
+
+
+//get all items
+
+app.get('/todos',async(req,res)=> 
+{
+
+    try{
+        const todos =await Todomodel.find();
+        res.json(todos);
+    }
+
+    catch (err){
+        res.status(500).json({message:err.message});
+    }
+ 
+});
+
+//update a tofo item
+
+app.put('/todos/:id', async (req,res)=>
+{
+    try{
+          const {title,description}=req.body;
+      const id = req.params.id;
+
+      const updatedtodo =  await Todomodel.findByIdAndUpdate(
+        id,
+        {title,description}
+      )
+
+      if(!updatedtodo)
+      {
+        
+        return res.status(404).json({message:'todo not found'});
+      }
+    }
+    catch(err)
+    {
+        console.log('error updating todo', err);
+        res.status(500).json({message:err.message});
+    }
+     
+
+
+});
+
+
 
     
 //set the port
