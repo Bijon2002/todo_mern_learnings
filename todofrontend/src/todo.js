ddad38c0227ca9b0c562853d8f1e758f061559b5
bijon2002 <bijosilin02@gmail.com>
2025-12-13 01:57:10 +0530

    errors ack

--- todofrontend/src/todo.js
+++ todofrontend/src/todo.js
@@ -1,4 +1,49 @@
+import e from "express";
+import { set } from "mongoose";
+import { useState } from "react";
+const apiurl = "http://localhost:3000/todos";
+
 export default function Todo() {
+
+    const [title, settitle] = useState("");
+    const [description, setdescription] = useState("");
+    const [todos, settodos] = useState([]);
+    const [error,setError]= useState("");
+    const [message,setMessage]=useState("");
+
+    const handlesubmit=()=>
+    {
+        //check inout values
+        if(title.trim() !=="" && description.trim() !=="")
+        {
+
+                       fetch(apiurl+"/todos",   
+                {
+                    method:"POST",
+                    headers: {
+                        "Content-Type":"application/json"
+                    },
+                    body:JSON.stringify({title, description})       
+                }
+            
+          
+            .then((res)=>
+
+            {
+                if(res.ok)
+                {
+  settodos([...todos, {title, description}]);
+  setMessage("item addedd successfully");
+                }
+
+                else
+                {
+                    setError (" unable to create todos item");}
+                }
+            }
+            //add todo item
+   
+
     return (
         <>
         <div className="row p-3 bg-success text-light" >
@@ -7,13 +52,19 @@ export default function Todo() {
 
         <div className="row p-3">
             <h3>add item</h3>
-            <p className="text-success">item addedd successfully</p>
-            <input className="form-control" type ="text" placeholder="title"/>
-            
+           {message && <p className="text-success">{message}</p>}
 
+            <div className="form-group d-flex gap-2">
+            <input className="form-control"onChange={(e)=> settitle(e.target.value)} type ="text" placeholder="title" value={title}/>
+             <input className="form-control" type ="text"  onChange={(e)=> setdescription(e.target.value)} placeholder="description" value={description}/>
+            <button className="btn btn-dark" onClick={handlesubmit}>Add Todo</button>
+
+            </div>
+
+          {error &&  <p>{error}</p>}
             </div>
 
 
 </>
  );
-}
+}  
